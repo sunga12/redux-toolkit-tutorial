8f5014e0b4144fa9b290a3c0a175c6f3ee5b12ca
Sunga Thawethe <sungabt@gmail.com>
2023-06-21 14:26:53 +0200

    Redux-toolkit-tutorial-complete

---
+++ starter/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import ReactDOM from 'react-dom';
+// import store and provider
+import { store } from './store';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
